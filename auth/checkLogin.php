ea4d87c777787375b115c207e13bf6245bd881b6
Mengheng855 <mengheng.mh855@gmail.com>
2026-02-10 07:22:43 +0700

    login

--- auth/checkLogin.php
+++ auth/checkLogin.php
@@ -1,13 +1,19 @@
 <?php 
     require '../connection.php';
+    session_start();
     if(isset($_POST['login'])){
         $email=$_POST['email'];
         $password=$_POST['password'];
-        $select="SELECT password FROM tbl_user WHERE email='$email'";
+        $select="SELECT password,is_admin FROM tbl_user WHERE email='$email'";
         $ex=mysqli_query($conn,$select);
         $row=mysqli_fetch_assoc($ex);
         if($password==$row['password']){
-            header('location:../user/user.php');
+            $_SESSION['is_admin']=$row['is_admin'];
+            if($row['is_admin']==1){
+                header('location:../admin/dashboard.php');
+            }else{
+                header('location:../user/user.php');
+            }
         }else{
             echo '<script>alert("khos...!")</script>';
         }
--- user/user.php
+++ user/user.php
@@ -1,5 +1,18 @@
-<?php include '../page/header.php' ?>
+<?php 
+include '../page/header.php';
+session_start();
+?>
     <h1>welcome to user</h1>
-    <a href="../auth/login.php" class="btn btn-primary">Login</a>
-    <a href="../auth/register.php" class="btn btn-primary">Register</a>
+    <?php 
+    if(isset($_SESSION['is_admin'])){
+        echo '
+            <a href="../auth/logout.php" class="btn btn-primary">logout</a>
+        ';
+    }else{
+        echo '
+            <a href="../auth/login.php" class="btn btn-primary">Login</a>
+            <a href="../auth/register.php" class="btn btn-primary">Register</a>
+        ';
+    }
+     ?>
 <?php include '../page/footer.php' ?>
